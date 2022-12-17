40d3e3db49b1e33a4f2de449985b8db53efde377
Alejandro Gomez Echavarria <alejandrogomezechavarria@gmail.com>
2022-12-17 18:48:52 -0500

    Fix bug in LICESE file

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -8,7 +8,7 @@ RESET_ALL = "\x1b[0m"
 
 initialize_git_repository = "{{ cookiecutter.initialize_git_repository }}"
 
-if(initialize_git_repository == 'yes'):
+if(initialize_git_repository == 'Yes'):
     print(f"{MESSAGE_COLOR}Initializing a git repository...{RESET_ALL}")
 
     subprocess.call(['git', 'init'])
